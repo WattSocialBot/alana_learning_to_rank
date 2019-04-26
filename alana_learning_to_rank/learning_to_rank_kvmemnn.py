9f84aea3fa24637db1421bb81d35e803a56205a7
Igor Shalyminov <ishalyminov@gmail.com>
2019-04-26 22:54:51 +0100

    train/eval loop implemented

--- alana_learning_to_rank/learning_to_rank_kvmemnn.py
+++ alana_learning_to_rank/learning_to_rank_kvmemnn.py
@@ -45,14 +45,19 @@ def float_tensor_type(use_gpu, **kwargs):
     return torch.cuda.FloatTensor if use_gpu else torch.FloatTensor
 
 
-def train(in_model, in_trainset, in_vocab, batch_size, num_epochs, truncate, cosine_loss_margin, learning_rate, optimizer, **kwargs):
+def train(in_model, in_trainset, in_devset, batch_size, num_epochs, truncate, cosine_loss_margin, learning_rate, optimizer, ckpt_steps, early_stopping_threshold, **kwargs):
     long_tensor_t = long_tensor_type(**kwargs)
     float_tensor_t = float_tensor_type(**kwargs)
     opt = getattr(torch.optim, optimizer)(in_model.parameters(), lr=learning_rate)
     in_model.train()
-    for epoch in range(num_epochs):
-        batch_gen = batch_generator(in_trainset, batch_size=batch_size)
-        print('Epoch {}'.format(epoch))
+
+    
+    batch_gen = batch_generator(in_trainset, batch_size=batch_size, rotate=True)
+    best_dev_loss = np.inf
+    epochs_without_improvement = 0
+    for epoch_idx in range(num_epochs):
+        print('Epoch {}'.format(epoch_idx + 1))
+
         for batch_idx, batch in enumerate(batch_gen):
             print('Processing batch {}\r'.format(batch_idx), end='')
             opt.zero_grad()
@@ -72,7 +77,51 @@ def train(in_model, in_trainset, in_vocab, batch_size, num_epochs, truncate, cos
             loss = torch.nn.CosineEmbeddingLoss(margin=cosine_loss_margin, reduce='sum')(xe.view(-1, emb_size), ye.view(-1, emb_size), y.view(-1))
             loss.backward()
             opt.step()
-    print('')
+
+            if batch_idx % ckpt_steps == 0:
+                break
+            print('')
+        in_model.eval()
+        trn_loss = evaluate(in_model, in_trainset, batch_size, truncate, cosine_loss_margin, **kwargs)
+        dev_loss = evaluate(in_model, in_devset, batch_size, truncate, cosine_loss_margin, **kwargs)
+        in_model.train()
+        print('Train loss: {:.3f}, dev loss {:.3f}'.format(trn_loss, dev_loss))
+        if 1e-3 < abs(best_dev_loss - dev_loss):
+            best_dev_loss = dev_loss
+            print('New best dev loss')
+            epochs_without_improvement = 0
+        else:
+            epochs_without_improvement += 1
+        if epochs_without_improvement == early_stopping_threshold:
+            print('Early stopping after {} epochs'.format(epochs_without_improvement))
+            break
+
+
+def evaluate(in_model, in_devset, batch_size, truncate, cosine_loss_margin, **kwargs):
+    long_tensor_t = long_tensor_type(**kwargs)
+    float_tensor_t = float_tensor_type(**kwargs)
+
+    batch_gen = batch_generator(in_devset, batch_size=batch_size)
+    losses = []
+    for batch_idx, batch in enumerate(batch_gen):
+        print('Processing batch {}\r'.format(batch_idx), end='')
+
+        xs, mems, ys, cands = batch
+
+        xs = tf.keras.preprocessing.sequence.pad_sequences(xs, truncate)
+        mems = pad_3d_batch(mems, truncate, **kwargs)
+        cands = pad_3d_batch(cands, truncate, **kwargs)
+        ys = tf.keras.preprocessing.sequence.pad_sequences(ys, truncate)
+        xe, ye = in_model(Variable(long_tensor_t(xs)),
+                          Variable(long_tensor_t(mems)),
+                          Variable(long_tensor_t(ys)),
+                          Variable(long_tensor_t(cands)))
+        y = Variable(float_tensor_t(xe.shape[:-1]).fill_(-1.0))
+        y[:,0]= 1
+        emb_size = xe.shape[-1]
+        loss = torch.nn.CosineEmbeddingLoss(margin=cosine_loss_margin, reduce='sum')(xe.view(-1, emb_size), ye.view(-1, emb_size), y.view(-1))
+        losses.append(loss.tolist())
+    return np.mean(np.array(losses))
 
 
 def make_dataset(in_table, in_rev_vocab, config, use_sample_weights=True):
@@ -110,8 +159,9 @@ def make_training_data(in_train, in_dev, in_test, in_config):
     word_vocab, rev_word_vocab = build_vocabulary(utterances_tokenized, max_size=in_config['max_vocab_size'])
 
     in_config['vocab_size'] = len(rev_word_vocab)
-    xs, mems, ys, cands = make_dataset(in_train, rev_word_vocab, in_config)
-    return word_vocab, rev_word_vocab, (xs, mems, ys, cands)
+    xs_trn, mems_trn, ys_trn, cands_trn = make_dataset(in_train, rev_word_vocab, in_config)
+    xs_dev, mems_dev, ys_dev, cands_dev = make_dataset(in_dev, rev_word_vocab, in_config)
+    return word_vocab, rev_word_vocab, (xs_trn, mems_trn, ys_trn, cands_trn), (xs_dev, mems_dev, ys_dev, cands_dev)
 
 
 def build_argument_parser():
@@ -131,14 +181,16 @@ if __name__ == '__main__':
     CONFIG = get_config(args.config)
 
     trainset = pd.read_json(args.trainset).sample(frac=1).reset_index(drop=True)
-    gold_trainset = trainset[trainset.target == 1.0][:100]
+    devset = pd.read_json(args.devset)
+    gold_trainset = trainset[trainset.target == 1.0]
+    gold_devset = devset[devset.target == 1.0]
 
     if not os.path.exists(args.model_folder):
         os.makedirs(args.model_folder)
-    word_vocab, rev_word_vocab, (xs, mems, ys, cands) = make_training_data(gold_trainset, gold_trainset, gold_trainset, CONFIG)
+    word_vocab, rev_word_vocab, data_trn, data_dev = make_training_data(gold_trainset, gold_devset, gold_trainset, CONFIG)
     model = create_model(CONFIG)
     print('Training with config "{}" :'.format(args.config))
     print(json.dumps(CONFIG, indent=2))
 
-    train(model, (xs, mems, ys, cands), word_vocab, **CONFIG)
+    train(model, data_trn, data_dev, **CONFIG)
 
